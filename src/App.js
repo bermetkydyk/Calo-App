6769ef6056c22b5a5c65ce1b97c1e1717c11bf64
bermetkydyk <38230942+bermetkydyk@users.noreply.github.com>
2021-04-21 18:11:36 -0400

    added authentication

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import Profile from './components/Profile';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <>
+        <LoginButton />
+        <LogoutButton />
+        <Profile />
+      </>
       <Switch>
         <Route path='/' />
       </Switch>
